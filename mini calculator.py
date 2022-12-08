8df0e97e30c511fa94d21b00e05859cf2d34ff02
Yash Shukla <119918899+yash7399@users.noreply.github.com>
2022-12-08 22:00:35 +0530

    Add files via upload

---
+++ mini calculator.py
@@ -0,0 +1,16 @@
+a=float(input("enter a number a="))
+b=float(input(" enter a number b ="))
+c=a+b
+print (" addition of the number is",c)
+d= a-b
+print (" substraction of the number is",d)
+e=a*b
+print (" multiplication of the number is",e)
+f=a/b
+print (" division of the number is",f)
+g=a%b
+print (" modulus of the number is",g)
+h=a**b
+print (" exponential of the number is",h)
+i=a//b
+print (" floor division of the number is",i)
